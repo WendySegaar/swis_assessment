b2cb59e1aa80b2f197ae3b3ed8fcb067becb9329
WendySegaar <1035706@mborijnland.nl>
2020-11-10 17:47:51 +0100

    Create Regions Migration

---
+++ database/migrations/2020_11_10_164154_create_regions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('region', 50);
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+
+            $table->bigInteger('schoolholiday_id');
+            $table->foreign('schoolholiday_id')->references('schoolholiday_id')->on('schoolholidays');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
