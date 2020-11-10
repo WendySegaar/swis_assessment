9d380ff2aa0ce9f854ba68b78ae9a0fd467331d8
WendySegaar <1035706@mborijnland.nl>
2020-11-10 17:48:01 +0100

    Create content migration

---
+++ database/migrations/2020_11_10_160953_create_content_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContentTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('content', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->text('notice')->nullable();
+            $table->string('license')->nullable();
+            $table->string('authorities')->nullable();
+            $table->string('rightholders')->nullable();
+            $table->string('location', 20)->nullable();
+            $table->dateTime('last_modified')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('content');
+    }
+}
