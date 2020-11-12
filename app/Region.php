a1a2404aeb3b2658fa36bfaa56a1bc77382746e1
WendySegaar <1035706@mborijnland.nl>
2020-11-12 15:27:13 +0100

    Update Region model with schoolholiday relationship and formatdate method

--- app/Region.php
+++ app/Region.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Region extends Model
 {
@@ -14,4 +15,22 @@ class Region extends Model
         'end_date',
         'school_holiday_id'
     ];
+
+    /**
+     * Region belong to SchoolHoliday
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function school_holiday()
+    {
+        return $this->belongsTo(SchoolHoliday::class, 'school_holiday_id', 'school_holiday_id');
+    }
+
+
+    public function getFormattedDateAttribute()
+    {
+        $start_date = Carbon::parse($this->start_date)->locale('nl')->translatedFormat('d F Y');
+        $end_date = Carbon::parse($this->end_date)->locale('nl')->translatedFormat('d F Y');
+        return "{$start_date} - {$end_date}";
+    }
 }
